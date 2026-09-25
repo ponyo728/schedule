3ce796e605bf4ac87098701e98ab2956a911654b
ponyo728 <yutota0728@gmail.com>
2026-09-25 15:26:17 +0900

    scheduleテーブルを作成

--- db/migrate/20260925060522_create_schedules.rb
+++ db/migrate/20260925060522_create_schedules.rb
@@ -1,6 +1,12 @@
 class CreateSchedules < ActiveRecord::Migration[7.1]
   def change
     create_table :schedules do |t|
+      t.date :date, null: false
+      t.string :title, null: false
+      t.time :start_time, null: false
+      t.time :end_time, null: false
+      t.text :detail, null: true
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_09_17_044459) do
+ActiveRecord::Schema[7.1].define(version: 2026_09_25_060522) do
+  create_table "schedules", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.date "date", null: false
+    t.string "title", null: false
+    t.time "start_time", null: false
+    t.time "end_time", null: false
+    t.text "detail"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_schedules_on_user_id"
+  end
+
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -23,4 +35,5 @@ ActiveRecord::Schema[7.1].define(version: 2026_09_17_044459) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "schedules", "users"
 end
